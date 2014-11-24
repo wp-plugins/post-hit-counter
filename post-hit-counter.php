7ee998ee23f057f12c7a0f74ffc328ba6440f87e
hlashbrooke <hlashbrooke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-24 13:39:19 +0000

    Post Hit Counter v1.1
    git-svn-id: https://plugins.svn.wordpress.org/post-hit-counter/trunk@1031781 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-hit-counter.php
+++ post-hit-counter.php
@@ -1,13 +1,13 @@
 <?php
 /*
  * Plugin Name: Post Hit Counter
- * Version: 1.0
+ * Version: 1.1.0
  * Plugin URI: https://wordpress.org/plugins/post-hit-counter/
  * Description: A counter to track how many times your posts are viewed.
  * Author: Hugh Lashbrooke
  * Author URI: http://www.hughlashbrooke.com/
  * Requires at least: 4.0
- * Tested up to: 4.0
+ * Tested up to: 4.0.1
  *
  * Text Domain: post-hit-counter
  * Domain Path: /lang/
@@ -21,6 +21,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Load plugin class files
 require_once( 'includes/class-post-hit-counter.php' );
+require_once( 'includes/class-post-hit-counter-settings.php' );
+require_once( 'includes/lib/class-post-hit-counter-admin-api.php' );
 
 /**
  * Returns the main instance of Post_Hit_Counter to prevent the need to use globals.
@@ -29,7 +31,12 @@ require_once( 'includes/class-post-hit-counter.php' );
  * @return object Post_Hit_Counter
  */
 function Post_Hit_Counter () {
-	$instance = Post_Hit_Counter::instance( __FILE__, '1.0.0' );
+	$instance = Post_Hit_Counter::instance( __FILE__, '1.1.0' );
+
+	if( is_null( $instance->settings ) ) {
+		$instance->settings = Post_Hit_Counter_Settings::instance( $instance );
+	}
+
 	return $instance;
 }
 
